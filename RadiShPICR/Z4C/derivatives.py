8b9ae643d11855e9c751aa22af80e228918573f8
Christopher Woolford <clwoolford@wisc.edu>
2026-07-08 08:57:05 -0500

    fixed ghost cells!

--- RadiShPICR/Z4C/derivatives.py
+++ RadiShPICR/Z4C/derivatives.py
@@ -7,23 +7,14 @@ def first_derivative(field, dr, parity=-1):
     Nr = field.shape[0]
     # get the number of grid points
 
-    # print("First Derivative 1")
-
     field_ = jnp.zeros(shape=(Nr+2)) # initialize dummy array
     field_ = field_.at[2:].set(field) # add field
 
-    # print("First Derivative 2")
-
-    field_ = field_.at[:2].set( field_[2:4] * parity ) # reflect the field across the inner boundary with parity value
-
-
-    # print("First Derivative 3")
+    field_ = field_.at[0].set(field_[2] * parity)  # set the first point using parity
+    field_ = field_.at[1].set(field_[3] * parity)  # set the second point using parity
 
     derivative  = (-jnp.roll(field_, -2) + 8 * jnp.roll(field_, -1) - 8 * jnp.roll(field_, 1) + jnp.roll(field_, 2)) / (12 * dr)
     # define the fourth order finite difference for the first derivative
-
-
-    # print("First Derivative 4")
 
     derivative = derivative.at[-1].set(0)
     derivative = derivative.at[-2].set(0)
@@ -41,7 +32,9 @@ def second_derivative(field, dr, parity=-1):
 
     field_ = jnp.zeros(shape=(Nr+2)) # initialize dummy array
     field_ = field_.at[2:].set(field) # add field
-    field_ = field_.at[:2].set( field_[2:4] * parity ) # reflect the field across the inner boundary with parity value
+
+    field_ = field_.at[0].set(field_[2] * parity)  # set the first point using parity
+    field_ = field_.at[1].set(field_[3] * parity)  # set the second point using parity
 
     derivative = (-jnp.roll(field_, -2) + 16 * jnp.roll(field_, -1) - 30 * field_ + 16 * jnp.roll(field_, 1) - jnp.roll(field_, 2)) / (12 * dr ** 2)
     # define the fourth order finite difference for the second derivative
@@ -62,7 +55,10 @@ def sixth_derivative(field, dr, parity=-1):
 
     field_ = jnp.zeros(shape=(Nr+3)) # initialize dummy array
     field_ = field_.at[3:].set(field) # add field
-    field_ = field_.at[:3].set( field_[3:6] * parity ) # reflect the field across the inner boundary with parity value
+    
+    field_ = field_.at[0].set(field_[3] * parity)  # set the first point using parity
+    field_ = field_.at[1].set(field_[4] * parity)  # set the second point using parity
+    field_ = field_.at[2].set(field_[5] * parity)  # set the third point using parity
 
     derivative =  (jnp.roll(field_, -3) - 6 * jnp.roll(field_, -2) + 15 * jnp.roll(field_, -1) - 20 * field_ + 15 * jnp.roll(field_, 1) - 6 * jnp.roll(field_, 2) + jnp.roll(field_, 3)) / (dr ** 6)
     # define the 6th order finite difference for the 6th derivative
--- RadiShPICR/Z4C/utils.py
+++ RadiShPICR/Z4C/utils.py
@@ -19,6 +19,8 @@ def trace_free_curvature(Arr, At, metric: Z4C_Metric):
     return Arr_trace_free, At_trace_free
 
 def generate_r_grid(r_min, r_max, Nr):
-    r_grid = jnp.linspace(r_min, r_max, Nr)
-    r_grid += (r_grid[1] - r_grid[0]) / (2 * Nr)  # shift cells so cell center is at dr/2
+    dr = (r_max - r_min) / Nr 
+    # get the radial grid spacing
+    r_grid = r_min + (jnp.arange(Nr) + 0.5) * dr
+    # shift the grid points to be at the center of each cell
     return r_grid
