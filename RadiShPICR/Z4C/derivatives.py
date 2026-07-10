91f2797005743903d2caf0ff31ec3594335bbf84
Christopher Woolford <clwoolford@wisc.edu>
2026-07-10 04:04:46 -0500

    fixing bug in ghost cells

--- RadiShPICR/Z4C/derivatives.py
+++ RadiShPICR/Z4C/derivatives.py
@@ -10,8 +10,8 @@ def first_derivative(field, dr, parity=-1):
     field_ = jnp.zeros(shape=(Nr+2)) # initialize dummy array
     field_ = field_.at[2:].set(field) # add field
 
-    field_ = field_.at[0].set(field_[2] * parity)  # set the first point using parity
-    field_ = field_.at[1].set(field_[3] * parity)  # set the second point using parity
+    field_ = field_.at[0].set(field_[3] * parity)  # set the first point using parity
+    field_ = field_.at[1].set(field_[2] * parity)  # set the second point using parity
 
     derivative  = (-jnp.roll(field_, -2) + 8 * jnp.roll(field_, -1) - 8 * jnp.roll(field_, 1) + jnp.roll(field_, 2)) / (12 * dr)
     # define the fourth order finite difference for the first derivative
@@ -33,8 +33,8 @@ def second_derivative(field, dr, parity=-1):
     field_ = jnp.zeros(shape=(Nr+2)) # initialize dummy array
     field_ = field_.at[2:].set(field) # add field
 
-    field_ = field_.at[0].set(field_[2] * parity)  # set the first point using parity
-    field_ = field_.at[1].set(field_[3] * parity)  # set the second point using parity
+    field_ = field_.at[0].set(field_[3] * parity)  # set the first point using parity
+    field_ = field_.at[1].set(field_[2] * parity)  # set the second point using parity
 
     derivative = (-jnp.roll(field_, -2) + 16 * jnp.roll(field_, -1) - 30 * field_ + 16 * jnp.roll(field_, 1) - jnp.roll(field_, 2)) / (12 * dr ** 2)
     # define the fourth order finite difference for the second derivative
@@ -56,9 +56,9 @@ def sixth_derivative(field, dr, parity=-1):
     field_ = jnp.zeros(shape=(Nr+3)) # initialize dummy array
     field_ = field_.at[3:].set(field) # add field
     
-    field_ = field_.at[0].set(field_[3] * parity)  # set the first point using parity
+    field_ = field_.at[0].set(field_[5] * parity)  # set the first point using parity
     field_ = field_.at[1].set(field_[4] * parity)  # set the second point using parity
-    field_ = field_.at[2].set(field_[5] * parity)  # set the third point using parity
+    field_ = field_.at[2].set(field_[3] * parity)  # set the third point using parity
 
     derivative =  (jnp.roll(field_, -3) - 6 * jnp.roll(field_, -2) + 15 * jnp.roll(field_, -1) - 20 * field_ + 15 * jnp.roll(field_, 1) - 6 * jnp.roll(field_, 2) + jnp.roll(field_, 3)) / (dr ** 6)
     # define the 6th order finite difference for the 6th derivative
